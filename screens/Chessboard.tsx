90a0fc3ea7eaeb1367501f00ae2be5ebfbb1c480
Lexa <axel.reviron@gmail.com>
2023-04-18 15:29:06 +0200

    placement des pieces

--- screens/Chessboard.tsx
+++ screens/Chessboard.tsx
@@ -1,9 +1,92 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
+import { Pieces } from '../entities';
 
 export default function Chessboard() {
   // Définir la taille de chaque case de l'échiquier
   const squareSize = 40;
+  const positions:Pieces[][] | null = [
+    [
+    {name : 'TourNoir', src: require('../assets/br.png')},
+    {name : 'CavalierNoir', src: require('../assets/bn.png')},
+    {name : 'FouNoir', src: require('../assets/bb.png')},
+    {name : 'ReineNoir', src: require('../assets/bq.png')},
+    {name : 'RoiNoir', src: require('../assets/bk.png')},
+    {name : 'FouNoir', src: require('../assets/bb.png')},
+    {name : 'CavalierNoir', src: require('../assets/bn.png')},
+    {name : 'TourNoir', src: require('../assets/br.png')}
+    ],
+    [
+      {name : 'PionNoir', src: require('../assets/bp.png')},
+      {name : 'PionNoir', src: require('../assets/bp.png')},
+      {name : 'PionNoir', src: require('../assets/bp.png')},
+      {name : 'PionNoir', src: require('../assets/bp.png')},
+      {name : 'PionNoir', src: require('../assets/bp.png')},
+      {name : 'PionNoir', src: require('../assets/bp.png')},
+      {name : 'PionNoir', src: require('../assets/bp.png')},
+      {name : 'PionNoir', src: require('../assets/bp.png')}
+    ],
+    [
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null}
+    ],
+    [
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null}
+    ],
+    [
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null}
+    ],
+    [
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null},
+      {name : '', src: null}
+    ],
+    [
+      {name : 'PionBlanc', src: require('../assets/wp.png')},
+      {name : 'PionBlanc', src: require('../assets/wp.png')},
+      {name : 'PionBlanc', src: require('../assets/wp.png')},
+      {name : 'PionBlanc', src: require('../assets/wp.png')},
+      {name : 'PionBlanc', src: require('../assets/wp.png')},
+      {name : 'PionBlanc', src: require('../assets/wp.png')},
+      {name : 'PionBlanc', src: require('../assets/wp.png')},
+      {name : 'PionBlanc', src: require('../assets/wp.png')}
+    ],
+    [
+      {name : 'TourBlanc', src: require('../assets/wr.png')},
+      {name : 'CavalierBlanc', src: require('../assets/wn.png')},
+      {name : 'FouBlanc', src: require('../assets/wb.png')},
+      {name : 'ReineBlanc', src: require('../assets/wq.png')},
+      {name : 'RoiBlanc', src: require('../assets/wk.png')},
+      {name : 'FouBlanc', src: require('../assets/wb.png')},
+      {name : 'CavalierBlanc', src: require('../assets/wn.png')},
+      {name : 'TourBlanc', src: require('../assets/wr.png')}
+      ],
+  ]
 
   // Définir les couleurs pour les cases claires et sombres
   const lightSquareColor = '#F0D9B5';
@@ -16,29 +99,36 @@ export default function Chessboard() {
       // Déterminer la couleur de la case en fonction de sa position sur l'échiquier
       const backgroundColor = (row + col) % 2 === 0 ? lightSquareColor : darkSquareColor;
       
-      switch (row) {
-        case 1 :
           squares.push(
             <View key={`${row}-${col}`} style={[styles.square, {backgroundColor, width: squareSize, height: squareSize}]}>
-              <Image source={require('../assets/bp.png')} style={{width: 40, height: 40}}/>
+              {<Image source={positions[row][col].src} style={{width: 40, height: 40}}/>}
+              {/* <Text style={styles.text}>{`${row}${col}`}</Text> */}
             </View>
           );
-          break;
-        case 6 :
-          squares.push(
-            <View key={`${row}-${col}`} style={[styles.square, {backgroundColor, width: squareSize, height: squareSize}]}>
-              <Image source={require('../assets/wp.png')} style={{width: 40, height: 40}}/>
-            </View>
-          );
-          break;
-        default:
-          squares.push(
-            <View key={`${row}-${col}`} style={[styles.square, {backgroundColor, width: squareSize, height: squareSize}]}>
-              {/* Ajouter le texte pour représenter la position de la case */}
-              <Text style={styles.text}>{`${row}${col}`}</Text>
-            </View>
-          );
-      }
+
+      // switch (row) {
+      //   case 1 :
+      //     squares.push(
+      //       <View key={`${row}-${col}`} style={[styles.square, {backgroundColor, width: squareSize, height: squareSize}]}>
+      //         <Image source={require('../assets/bp.png')} style={{width: 40, height: 40}}/>
+      //       </View>
+      //     );
+      //     break;
+      //   case 6 :
+      //     squares.push(
+      //       <View key={`${row}-${col}`} style={[styles.square, {backgroundColor, width: squareSize, height: squareSize}]}>
+      //         <Image source={require('../assets/wp.png')} style={{width: 40, height: 40}}/>
+      //       </View>
+      //     );
+      //     break;
+      //   default:
+      //     squares.push(
+      //       <View key={`${row}-${col}`} style={[styles.square, {backgroundColor, width: squareSize, height: squareSize}]}>
+      //         {/* Ajouter le texte pour représenter la position de la case */}
+      //         <Text style={styles.text}>{`${row}${col}`}</Text>
+      //       </View>
+      //     );
+      // }
     }
   }
 
