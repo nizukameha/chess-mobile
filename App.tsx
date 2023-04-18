07d7db4c8e63adcd1683d080b6923c022bf3813d
Lexa <axel.reviron@gmail.com>
2023-04-18 12:20:47 +0200

    chessboard

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import HomeScreen from './screens/HomeScreen';
+import Chess from './screens/Chess';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name="Accueil" component={HomeScreen} />
+            <Stack.Screen name="Chess" component={Chess} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+}
